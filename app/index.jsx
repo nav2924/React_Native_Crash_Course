0df7fad1879d13c085997c94dedfdd946ebb18d8
nav2924 <navi29012004@gmail.com>
2024-08-27 14:43:00 +0530

    Footer layout

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Link } from "expo-router";
-
+import { SafeAreaFrameContext } from "react-native-safe-area-context";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
